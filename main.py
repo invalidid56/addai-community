49a8c3b3149dfbcb03773b117d5e3f25dcfb483e
Junseo Kang <karl2003@naver.com>
2024-02-03 00:38:23 +0900

    fix: req

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from endpoint.user.route import router as user_router
 from endpoint.article.route import router as article_router
+from endpoint.comment.route import router as comment_router
 
 
 tags_metadata = [
@@ -37,6 +38,8 @@ app.add_middleware(
 
 app.include_router(user_router)
 app.include_router(article_router)
+app.include_router(comment_router)
+
 
 
 @app.on_event("startup")
